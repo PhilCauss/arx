65b038f08fb79859cb375c7f4b404f84e58f8d05
PhilCauss <42981905+PhilCauss@users.noreply.github.com>
2025-08-10 17:08:23 -0400

    1.0.7

--- src/arx/__init__.py
+++ src/arx/__init__.py
@@ -3,7 +3,7 @@ arx - A secure wrapper around yay package manager
 Analyzes packages for malicious intent before installation
 """
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 __author__ = "Arx Developer"
 
 from .analyzer import ArxSecurityAnalyzer
